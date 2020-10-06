919ec28fb81ad133a7a94c9ffab3165970593cee
Rajesh Mondal <rajesh01749@gmail.com>
2020-10-06 23:53:38 +0600

    working with complete and incomplete tasks

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ $connection = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, );
 if ( !$connection ) {
     throw new Exception( "Cannot connect to database" );
 }
-$query = "SELECT * FROM tasks ORDER BY date";
+$query = "SELECT * FROM tasks WHERE complete = 0 ORDER BY date";
 $result = mysqli_query( $connection, $query );
+
+$completeTasksQuery = "SELECT * FROM tasks WHERE complete = 1 ORDER BY date";
+$resultCompleteTasks = mysqli_query( $connection, $completeTasksQuery );
 
 ?>
 <!DOCTYPE html>
@@ -32,54 +35,89 @@ $result = mysqli_query( $connection, $query );
 <div class="container" id="main">
     <h2>Tasks Manager</h2>
     <p>This is a sample project for managing our daily tasks. We are going to use HTML, CSS, PHP, MySQL, jQuery for this project</p>
-        <h4>All Tasks</h4>
-        <?php 
-        if( mysqli_num_rows( $result )==0 ){
-            ?>
-            <p>No Task Found</p>
+    <?php 
+    if( mysqli_num_rows( $resultCompleteTasks ) > 0 ) {
+        ?>
+        <h4>Complete Tasks</h4>
+        <table>
+            <thead>
+            <tr>
+                <th></th>
+                <th>Id</th>
+                <th>Task</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
             <?php
-        }else{
-        ?>
-        <form>
-            <table>
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>Id</th>
-                    <th>Task</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                while($data = mysqli_fetch_assoc( $result ) ) {
-                    $timestamp = strtotime( $data['date'] );
-                    $date = date( "jS M, Y", $timestamp );
-                    ?>
-                    <tr>
-                        <td><input class="label-inline" type="checkbox" value="<?php echo $data['id']; ?>"></td>
-                        <td><?php echo $data['id']; ?></td>
-                        <td><?php echo $data['task']; ?></td>
-                        <td><?php echo $date; ?></td>
-                        <td><a href="#">Delete</a> | <a href="#">Edit</a> | <a href="#">Complete</a></td>
-                    </tr>
-                    <?php
-                }
-                mysqli_close( $connection );
+            while( $cdata = mysqli_fetch_assoc( $resultCompleteTasks ) ) {
+                $timestamp = strtotime( $cdata['date'] );
+                $cdate = date( "jS M, Y", $timestamp );
                 ?>
-                </tbody>
-            </table>
-            <select id="action">
-                <option value="0">With Selected</option>
-                <option value="delete">Delete</option>
-                <option value="complete">Mark As Complete</option>
-            </select>
-            <input class="button-primary" id="bulksubmit" type="submit" value="Submit">
-        </form>
+                <tr>
+                    <td><input class="label-inline" type="checkbox" value="<?php echo $cdata['id']; ?>"></td>
+                    <td><?php echo $cdata['id']; ?></td>
+                    <td><?php echo $cdata['task']; ?></td>
+                    <td><?php echo $cdate; ?></td>
+                    <td><a href="#">Delete</a> </td>
+                </tr>
+                <?php
+            }
+            ?>
+            </tbody>
+        </table>
+        <p>...</p>
         <?php
-        }
+    }
+    ?>
+        
+    <?php 
+    if( mysqli_num_rows( $result ) ==0 ){
         ?>
+        <p>No Task Found</p>
+        <?php
+    }else{
+    ?>
+    <h4>Upcomming Tasks</h4>
+    <table>
+        <thead>
+        <tr>
+            <th></th>
+            <th>Id</th>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        while($data = mysqli_fetch_assoc( $result ) ) {
+            $timestamp = strtotime( $data['date'] );
+            $date = date( "jS M, Y", $timestamp );
+            ?>
+            <tr>
+                <td><input class="label-inline" type="checkbox" value="<?php echo $data['id']; ?>"></td>
+                <td><?php echo $data['id']; ?></td>
+                <td><?php echo $data['task']; ?></td>
+                <td><?php echo $date; ?></td>
+                <td><a href="#">Delete</a> | <a href="#">Edit</a> | <a href="#">Complete</a></td>
+            </tr>
+            <?php
+        }
+        mysqli_close( $connection );
+        ?>
+        </tbody>
+    </table>
+    <select id="action">
+        <option value="0">With Selected</option>
+        <option value="delete">Delete</option>
+        <option value="complete">Mark As Complete</option>
+    </select>
+    <input class="button-primary" id="bulksubmit" type="submit" value="Submit">
+    <?php
+    }
+    ?>
     <p>...</p>
     <h4>Add Tasks</h4>
     <form method="post" action="tasks.php">
